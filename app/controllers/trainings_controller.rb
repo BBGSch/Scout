f5a58e037054997b49a35cb3d6027f7fbeebff5e
Daan Roelofs <daanroelofs599@gmail.com>
2020-09-01 12:42:28 +0200

    scoutcontrollers

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,12 +1,12 @@
 class TrainingsController < ApplicationController
   def show
     @training = Training.find(params[:id])
-    @training_session = Training_session.new
+    @training_session = User.new
   end
 
   def new
     @training = Training.new
-    @training.user = User.find(params[])
+    @training.user_id = User.find(params[])
   end
 
   def create
