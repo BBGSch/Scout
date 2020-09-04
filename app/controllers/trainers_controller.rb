4bf50c3dbe117b4e0683ebcf8d20f35eaf9a00de
Nathan Harris <nate.sam.harris@gmail.com>
2020-09-04 15:13:16 +0200

    search function

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,6 +1,21 @@
 class TrainersController < ApplicationController
+
   def index
-    @trainers = User.where(trainer: true)
+    @trainers = search(params)
+  end
+
+  def search(params)
+     return User.where(trainer: true) if params.keys.length == 2
+
+     sessions = TrainingSession.near(params[:location], 50)
+     trainers = sessions.map{|session| session.training.user}.uniq
+     genders = []
+     genders << "male" if params[:male] == "true"
+     genders << "female" if params[:female] == "true"
+     genders << "non-binary" if params[:other] == "true"
+     return trainers if genders.empty?
+     trainers.select { |trainer| genders.include?(trainer.gender)}
+     # && trainer.age >= params[:minage].to_i && trainer.age <= params[:maxage].to_i}
   end
 
   def show
